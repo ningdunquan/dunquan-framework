51f089af50e3096bef1cbcd08964d6340d2b1d54
dunquan.ning <dunquan.ning@dianping.com>
2016-02-28 21:51:23 +0800

    dunquan07

--- src/main/java/org/dunquan/framework/mvc/view/ViewResolver.java
+++ src/main/java/org/dunquan/framework/mvc/view/ViewResolver.java
@@ -11,6 +11,13 @@ import org.dunquan.framework.mvc.exception.DispatcherException;
  */
 public interface ViewResolver {
 
+	/**
+	 * 解析视图
+	 * @param executeContext
+	 * @param actionValue
+	 * @param actionHandler
+	 * @throws DispatcherException
+	 */
 	public void resolve(ExecuteContext executeContext, Object actionValue, ActionHandler actionHandler)
 			throws DispatcherException;
 }
